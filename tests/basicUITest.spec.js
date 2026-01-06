ba57a1303344ae20950c007dad6a39e391f5a062
Avinash Kunapareddy <kunapareddy.avi@gmail.com>
2026-01-06 16:41:44 +0530

    added assertion to validate page title for Google

--- tests/basicUITest.spec.js
+++ tests/basicUITest.spec.js
@@ -1,11 +1,14 @@
-const { test } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
 test('First Test Script in Playwright', async ({ browser }) => {
 	const context = await browser.newContext();
 	const page = await context.newPage();
 	await page.goto("https://playwright.dev/");
+	console.log(await page.title());
 })
 
 test('Test Script in Playwright without browser fixture and page function', async ({ page }) => {
 	await page.goto("https://google.com");
+	console.log(await page.title());
+	await expect(page).toHaveTitle("Google");
 })
