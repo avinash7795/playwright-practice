4198d0ef9b13fe85c3148e08b796bece611533c6
Avinash Kunapareddy <kunapareddy.avi@gmail.com>
2026-01-07 12:18:18 +0530

    added new test case to validate error message text in login practice page when given wrong credentials.

--- tests/basicUITest.spec.js
+++ tests/basicUITest.spec.js
@@ -7,8 +7,22 @@ test('First Test Script in Playwright', async ({ browser }) => {
 	console.log(await page.title());
 })
 
-test('Test Script in Playwright without browser fixture and page function', async ({ page }) => {
+test('Test Script in Playwright without browser and page fixtures', async ({ page }) => {
 	await page.goto("https://google.com");
 	console.log(await page.title());
 	await expect(page).toHaveTitle("Google");
+})
+
+test('Extracting error message and validating from web page', async ({ page }) => {
+	await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+	console.log(await page.title());
+	//used regular expression to match partial page title
+	await expect(page).toHaveTitle(/LoginPage/);
+	await page.locator("input#username").fill("avinash");
+	await page.locator("[type='password']").fill("learning");
+	await page.locator("input#signInBtn").click();
+	//extracting error message using textContent method and printing in console
+	console.log(await page.locator("[style*='block']").textContent());
+	//adding assertion to validate the partial text of error message
+	await expect(page.locator("[style*='block']")).toContainText("Incorrect");
 })
