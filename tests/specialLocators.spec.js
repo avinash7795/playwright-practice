a85ea577915507b0c81b259b949cf0acc311a1a8
Avinash Kunapareddy <kunapareddy.avi@gmail.com>
2026-01-19 18:12:11 +0530

    created locator chaining user css locator and special locators with the help of filter

--- tests/specialLocators.spec.js
+++ tests/specialLocators.spec.js
@@ -1,4 +1,5 @@
 const { test, expect } = require('@playwright/test');
+const { link } = require('node:fs');
 
 test('Playwright special locators using angular app', async ({ page }) => {
 	//navigating to a website
@@ -9,6 +10,23 @@ test('Playwright special locators using angular app', async ({ page }) => {
 	await page.getByLabel("Employed").check();
 	//locating static dropdown and selecting option
 	await page.getByLabel("Gender").selectOption("Male");
+	//locating password textbox and typing
+	await page.getByPlaceholder('Password').fill("abc123");
+	//locating Submit button using getByRole locator and clicking
+	await page.getByRole("button", { name: 'Submit' }).click();
+	//grabbing success message text using getByText locator and checking the visibility
+	const successMsgFlag = await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
+	//asserting the success message visibility is true
+	expect(successMsgFlag).toBeTruthy();
+	//locating Shop hyper-link using getByRole and clicking
+	await page.getByRole("link", { name: 'Shop' }).click();
+	//locator chaining using filter and getByRole to select a product and add to cart
+	await page.locator("app-card").filter({ hasText: 'Nokia Edge' }).getByRole("button").click();
+
+
+
+
+
 
 
 
