b774337a6f6086ae57b519fa746a8764a67c68bb
Avinash Kunapareddy <kunapareddy.avi@gmail.com>
2026-01-22 15:05:42 +0530

    automating alert pop-up click

--- tests/hiddenFunctionality.spec.js
+++ tests/hiddenFunctionality.spec.js
@@ -7,7 +7,18 @@ test('Hidden element functionality', async ({ page }) => {
     await page.goBack();
     await page.goForward();
     */
+
+    //assertion to make sure the text is visible before hiding
     await expect(page.locator('#displayed-text')).toBeVisible();
+    //clicking on hide button to hide the text
     await page.locator("#hide-textbox").click();
+    //assertion to make sure the text is hidden
     await expect(page.locator('#displayed-text')).toBeHidden();
+    //code to enable debugging
+    //await page.pause();
+    //listener to wait for alert to appear and clicking on ok
+    page.on('dialog', dialog => dialog.accept());
+    //clicking on "Confirm" button to display the alert
+    await page.locator("#confirmbtn").click();
+
 })
