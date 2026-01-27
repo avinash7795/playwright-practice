e13f4a13599e92ff952c4e19e646bf365176ec48
Avinash Kunapareddy <kunapareddy.avi@gmail.com>
2026-01-27 18:43:56 +0530

    creating order through API and continuing validation of orderId by refactoring the code

--- tests/webApiPart1.spec.js
+++ tests/webApiPart1.spec.js
@@ -1,8 +1,12 @@
 const { test, expect, request } = require('@playwright/test');
 //storing request payload in a variable
-const loginData = { userEmail: "kunapareddy.avi@gmail.com", userPassword: "Avinash@123" }
-//declaring a variable "token" as global one
+const loginData = { userEmail: "kunapareddy.avi@gmail.com", userPassword: "Avinash@123" };
+const orderPayload = { orders: [{ country: "India", productOrderedId: "6964a1cbc941646b7a91786b" }] };
+
+//declaring "token" as global variable
 let token;
+//declaring orderId as global variable
+let orderId;
 
 //beforeAll annotation will execute the code written in it before executing all test cases
 test.beforeAll(async () => {
@@ -21,6 +25,25 @@ test.beforeAll(async () => {
     const loginResponseJson = await loginResponse.json();
     //parsing and fetching the required json object called token and storing in global variable
     token = loginResponseJson.token;
+
+    //API call to create order
+    const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
+        {
+            //passing payload
+            data: orderPayload,
+            //passing request headers like token and content type
+            headers: {
+                'Authorization': token,
+                'Contnet-Type': 'application/json'
+            },
+        }
+    )
+    //converting javascript object to json object
+    const orderResponseJson = await orderResponse.json();
+    //fetching orderid from json object
+    orderId = orderResponseJson.orders[0];
+    //printing orderid in console
+    console.log(orderId);
 
 });
 
@@ -42,65 +65,6 @@ test('End to end work flow of ecommerce app ', async ({ page }) => {
 
     //navigating to ecommerce webpage
     await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
-    /*   await userName.fill(email);
-       await password.fill("Avinash@123");
-       await loginBtn.click(); */
-
-    //await page.waitForLoadState('networkidle'); //-- not working
-    //will wait until the last element is present
-    await cardTitles.first().waitFor();
-    //storing the text of all webelements in array
-    const allTitles = await cardTitles.allTextContents();
-    //printing all the product titles in console
-    console.log(allTitles);
-    //logic to fetch required product name from list of products
-    const count = await products.count();
-    console.log("products count: " + count);
-    for (let i = 0; i < count; i++) {
-        //used locator chaining concept to minimize the scope of search
-        if (await products.nth(i).locator("b").textContent() === productName) {
-            //used new locator concept 'text=text value in DOM' and added product to card
-            await products.nth(i).locator("text=  Add To Cart").click();
-            break;
-        }
-    }
-    //clicking on cart button
-    await cartBtn.click();
-    //wait mechanism to make sure all products in the cart are loaded because isVisible don't have auto-wait mechanism
-    await page.locator(".cart li").first().waitFor();
-    /*verifying the product 'ZARA COAT 3' is visible in the cart page
-    by using pseudo-class in the locator and isVisible method*/
-    const isPresent = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
-    //verifying the presence of product in cart page to be True
-    expect(isPresent).toBeTruthy();
-    //clicking on Checkout button
-    await page.locator("text=Checkout").click();
-    //typing "ind" word charecter by charecter in the auto suggestive dropdown
-    await page.locator("[placeholder*='Country']").pressSequentially("ind");
-    //waiting till the dropdown elements are loaded
-    await dropdown.waitFor();
-    //fetching the count of suggestive options from the dropdown
-    const optionsCount = await dropdown.locator("button").count();
-    //locating and clicking on dropwdown text "India"
-    for (let i = 0; i < optionsCount; i++) {
-        //fetching the text content from the nth element from auto suggestive options
-        const suggestedText = await dropdown.locator("button").nth(i).textContent();
-        if (suggestedText === " India") {
-            //clicking on "India" text
-            await dropdown.locator("button").nth(i).click();
-            break;
-        }
-    }
-    //validating the email used for login is present on the place order page
-    await expect(page.locator(".user__name label")).toHaveText(email);
-    //clicking on the Place Order button
-    await page.locator(".action__submit").click();
-    //validating the text "Thankyou for the order." is present in the thank you page
-    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
-    //grabing the order id from thank you page
-    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
-    //printing order id in the console
-    console.log(orderId);
     //clicking on "ORDERS" button
     await page.locator("button[routerlink*='myorders']").click();
     //waiting for order-details table to be loaded using external wait mechaninsm
@@ -118,6 +82,6 @@ test('End to end work flow of ecommerce app ', async ({ page }) => {
     }
     //grabing the text of order id from the order details page
     const orderIdDetails = await page.locator(".col-text").textContent();
-    //validating whether the order id from oder details page is matching with the original order id
+    //validating whether the order id from oder details page is matching with the original order id which was created through API
     expect(orderId.includes(orderIdDetails)).toBeTruthy();
 })
