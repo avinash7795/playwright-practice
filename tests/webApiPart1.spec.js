9272cfc7fe833f9e1d970e0732b3f49c26dfc97c
Avinash Kunapareddy <kunapareddy.avi@gmail.com>
2026-01-29 16:35:48 +0530

    refactored code to get response from util class

--- tests/webApiPart1.spec.js
+++ tests/webApiPart1.spec.js
@@ -1,53 +1,25 @@
 const { test, expect, request } = require('@playwright/test');
+const { ApiUtils } = require('./utils/ApiUtils');
+
 //storing request payload in a variable
 const loginData = { userEmail: "kunapareddy.avi@gmail.com", userPassword: "Avinash@123" };
 const orderPayload = { orders: [{ country: "India", productOrderedId: "6964a1cbc941646b7a91786b" }] };
 
-//declaring "token" as global variable
-let token;
-//declaring orderId as global variable
-let orderId;
+//declaring "response" as global variable
+let response;
 
 //beforeAll annotation will execute the code written in it before executing all test cases
 test.beforeAll(async () => {
     //providing new context without any previous state to the api call
     const apiContext = await request.newContext();
-    //calling POST method with required URL and passing the payload in data, storing the API response
-    const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
-        {
-            //passing the payload into the data param
-            data: loginData
-        }
-    )
-    //validating the API response is success using "ok" method
-    expect((loginResponse).ok()).toBeTruthy;
-    //converting javascript object to json object using .json() method
-    const loginResponseJson = await loginResponse.json();
-    //parsing and fetching the required json object called token and storing in global variable
-    token = loginResponseJson.token;
-
-    //API call to create order
-    const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
-        {
-            //passing payload
-            data: orderPayload,
-            //passing request headers like token and content type
-            headers: {
-                'Authorization': token,
-                'Contnet-Type': 'application/json'
-            },
-        }
-    )
-    //converting javascript object to json object
-    const orderResponseJson = await orderResponse.json();
-    //fetching orderid from json object
-    orderId = orderResponseJson.orders[0];
-    //printing orderid in console
-    console.log(orderId);
+    //creating object for ApiUtils class
+    const apiUtils = new ApiUtils(apiContext, loginData);
+    //storing method returnvalue in global variable 'response'
+    response = await apiUtils.getOrderId(orderPayload);
 
 });
 
-test('End to end work flow of ecommerce app ', async ({ page }) => {
+test('Validate order Id from orders page', async ({ page }) => {
     const email = "kunapareddy.avi@gmail.com";
     const userName = page.locator("input#userEmail");
     const password = page.locator("input#userPassword");
@@ -61,7 +33,7 @@ test('End to end work flow of ecommerce app ', async ({ page }) => {
     //javascript code to insert token value into browser local storage
     await page.addInitScript(value => {
         window.localStorage.setItem('token', value)
-    }, token);
+    }, response.token);
 
     //navigating to ecommerce webpage
     await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
@@ -74,7 +46,7 @@ test('End to end work flow of ecommerce app ', async ({ page }) => {
     //logic to get order-id from the first column of the table and to match with existing orderId
     for (let i = 0; i < await rows.count(); i++) {
         const orderIdtext = await rows.nth(i).locator("th").textContent();
-        if (orderId.includes(orderIdtext)) {
+        if (response.orderId.includes(orderIdtext)) {
             //clicking on "view" button of matching order id row
             await rows.nth(i).locator("button").first().click();
             break;
@@ -83,5 +55,5 @@ test('End to end work flow of ecommerce app ', async ({ page }) => {
     //grabing the text of order id from the order details page
     const orderIdDetails = await page.locator(".col-text").textContent();
     //validating whether the order id from oder details page is matching with the original order id which was created through API
-    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+    expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
 })
