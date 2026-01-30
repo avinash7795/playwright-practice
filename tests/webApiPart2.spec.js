97e2fc86898202afb9fbf1a0c89b8f3849a996ee
Avinash Kunapareddy <kunapareddy.avi@gmail.com>
2026-01-30 15:30:56 +0530

    injecting stored browser context in new browser page and bypassing login

---
+++ tests/webApiPart2.spec.js
@@ -0,0 +1,120 @@
+const { test, expect } = require('@playwright/test');
+
+//creating global variable to store the browser content like token
+let webContext;
+//this global variable is used in the below 2 test methods
+const email = "kunapareddy.avi@gmail.com";
+
+//this method is used to write logic that should be executed to before all test methods
+test.beforeAll(async ({ browser }) => {
+
+
+    //creating new browser context
+    const context = await browser.newContext();
+    //creates a new page in the browser context
+    const page = await context.newPage();
+
+    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+
+    const userName = page.locator("input#userEmail");
+    const password = page.locator("input#userPassword");
+    const loginBtn = page.locator("input#login");
+    const cardTitles = page.locator(".card-body b");
+
+
+    await userName.fill(email);
+    await password.fill("Avinash@123");
+    await loginBtn.click();
+    //will wait until the last element is present
+    await cardTitles.first().waitFor();
+    //storing browser content like token in 'state.json' file
+    await context.storageState({ path: 'state.json' });
+    //creating new browser context by injecting stored browser contents
+    webContext = await browser.newContext({ storageState: 'state.json' });
+})
+
+test('End to end work flow of ecommerce app', async () => {
+
+    //created new page on top of browser context in which the login details like token is injected
+    const page = await webContext.newPage();
+
+    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+
+    const cardTitles = page.locator(".card-body b");
+    const products = page.locator(".card-body");
+    const productName = 'ZARA COAT 3';
+    const cartBtn = page.locator("[routerlink*='cart']");
+    const dropdown = page.locator(".ta-results");
+
+    //storing the text of all webelements in array
+    const allTitles = await cardTitles.allTextContents();
+    //printing all the product titles in console
+    console.log(allTitles);
+    //logic to fetch required product name from list of products
+    const count = await products.count();
+    console.log("products count: " + count);
+    for (let i = 0; i < count; i++) {
+        //used locator chaining concept to minimize the scope of search
+        if (await products.nth(i).locator("b").textContent() === productName) {
+            //used new locator concept 'text=text value in DOM' and added product to card
+            await products.nth(i).locator("text=  Add To Cart").click();
+            break;
+        }
+    }
+    //clicking on cart button
+    await cartBtn.click();
+    //wait mechanism to make sure all products in the cart are loaded because isVisible don't have auto-wait mechanism
+    await page.locator(".cart li").first().waitFor();
+    /*verifying the product 'ZARA COAT 3' is visible in the cart page
+    by using pseudo-class in the locator and isVisible method*/
+    const isPresent = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+    //verifying the presence of product in cart page to be True
+    expect(isPresent).toBeTruthy();
+    //clicking on Checkout button
+    await page.locator("text=Checkout").click();
+    //typing "ind" word charecter by charecter in the auto suggestive dropdown
+    await page.locator("[placeholder*='Country']").pressSequentially("ind");
+    //waiting till the dropdown elements are loaded
+    await dropdown.waitFor();
+    //fetching the count of suggestive options from the dropdown
+    const optionsCount = await dropdown.locator("button").count();
+    //locating and clicking on dropwdown text "India"
+    for (let i = 0; i < optionsCount; i++) {
+        //fetching the text content from the nth element from auto suggestive options
+        const suggestedText = await dropdown.locator("button").nth(i).textContent();
+        if (suggestedText === " India") {
+            //clicking on "India" text
+            await dropdown.locator("button").nth(i).click();
+            break;
+        }
+    }
+    //validating the email used for login is present on the place order page
+    await expect(page.locator(".user__name label")).toHaveText(email);
+    //clicking on the Place Order button
+    await page.locator(".action__submit").click();
+    //validating the text "Thankyou for the order." is present in the thank you page
+    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+    //grabing the order id from thank you page
+    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    //printing order id in the console
+    console.log(orderId);
+    //clicking on "ORDERS" button
+    await page.locator("button[routerlink*='myorders']").click();
+    //waiting for order-details table to be loaded using external wait mechaninsm
+    await page.locator("tbody").waitFor();
+    //collecting rows count from the table
+    const rows = page.locator("tbody tr");
+    //logic to get order-id from the first column of the table and to match with existing orderId
+    for (let i = 0; i < await rows.count(); i++) {
+        const orderIdtext = await rows.nth(i).locator("th").textContent();
+        if (orderId.includes(orderIdtext)) {
+            //clicking on "view" button of matching order id row
+            await rows.nth(i).locator("button").first().click();
+            break;
+        }
+    }
+    //grabing the text of order id from the order details page
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    //validating whether the order id from oder details page is matching with the original order id
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+})
