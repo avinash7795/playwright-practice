130324e0c626beece1ac66ebc9b41c492f3d2c28
Avinash Kunapareddy <kunapareddy.avi@gmail.com>
2026-02-04 10:55:03 +0530

    taking screenshot and partial screenshot in the webpage for visual comparision

---
+++ tests/screenshotsScript.spec.js
@@ -0,0 +1,15 @@
+const { test, expect } = require('@playwright/test');
+
+test('Screenshot and Partial Screenshot functionality', async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    //assertion to make sure the text is visible before hiding
+    await expect(page.locator('#displayed-text')).toBeVisible();
+    //this will take the partial or web element level screenshot
+    await page.locator('#displayed-text').screenshot({ path: 'partialScreenshot.png' });
+    //clicking on hide button to hide the text
+    await page.locator("#hide-textbox").click();
+    //this will take full page screenshot and stores the file on project level
+    await page.screenshot({ path: 'screenshot.png' });
+    //assertion to make sure the text is hidden
+    await expect(page.locator('#displayed-text')).toBeHidden();
+});
