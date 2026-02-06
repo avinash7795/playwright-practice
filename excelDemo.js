b95ff2734ee27b9fa5c9f1722fc239f0b8a77c74
avinash7795 <51531667+avinash7795@users.noreply.github.com>
2026-02-06 12:29:45 +0530

    modified comments in the respective lines of code

--- excelDemo.js
+++ excelDemo.js
@@ -1,7 +1,7 @@
 //importing exceljs pacakge to class
 const ExcelJs = require('exceljs');
 
-//creating a function to print the values in excel file
+//creating a function to write the values in excel workbook by taking searchtext, replacetext, change object and filepath
 async function writeExcel(searchText, replaceText, change, filepath) {
 
     //creating object for ExcelJs class and accessing Workbook of excel file
@@ -10,8 +10,9 @@ async function writeExcel(searchText, replaceText, change, filepath) {
     await workbook.xlsx.readFile(filepath);
     //accessing first sheet of excel workbook
     const worksheet = workbook.getWorksheet('Sheet1');
+    //calling readExcel() function with worksheet and searchtext as argument and storing the returned output object in a variable
     const output = await readExcel(worksheet, searchText);
-    //getting cell location
+    //getting cell location by getting row num from output object and col num by incrementing with change object value
     const cell = worksheet.getCell(output.row, output.col + change.colChange);
     //replacing required value in cell
     cell.value = replaceText;
@@ -19,7 +20,7 @@ async function writeExcel(searchText, replaceText, change, filepath) {
     await workbook.xlsx.writeFile(filepath);
 }
 
-//calling excelTest function
+//creating readExcel() function by taking worksheet and searchText as arguments
 async function readExcel(worksheet, searchText) {
     //creating a object with row and column number with default values
     let output = { row: -1, col: -1 };
@@ -27,7 +28,7 @@ async function readExcel(worksheet, searchText) {
     worksheet.eachRow((row, rowNumber) => {
         //accessing cell by cell on iterating through column number using eachCell function
         row.eachCell((cell, colNumber) => {
-            //comparing cell value with required value 'Apple'
+            //comparing cell value with required searchText value
             if (cell.value === searchText) {
                 //storing rowNumber to row and colNumber to col in output object
                 output.row = rowNumber;
@@ -37,7 +38,9 @@ async function readExcel(worksheet, searchText) {
         })
 
     })
+    //returning output object with row and col numbers
     return output;
 }
 
+//calling writeExcel function
 writeExcel("Mango", 350, { rowChange: 0, colChange: 2 }, "C:/Users/avinash.kunapareddy/Downloads/excelDownloadTest.xlsx");
