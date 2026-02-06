9b5d0b7f1a6788dcca33d751b8e2de7d3083aaa6
avinash7795 <51531667+avinash7795@users.noreply.github.com>
2026-02-06 12:15:06 +0530

    modified code to segregate read and write opearations to excel workbook
    added script to change the price of a given fruit name

--- excelDemo.js
+++ excelDemo.js
@@ -2,21 +2,33 @@
 const ExcelJs = require('exceljs');
 
 //creating a function to print the values in excel file
-async function excelTest() {
-    //creating a object with row and column number with default values
-    let output = { row: -1, col: -1 };
+async function writeExcel(searchText, replaceText, change, filepath) {
+
     //creating object for ExcelJs class and accessing Workbook of excel file
     const workbook = new ExcelJs.Workbook();
     //reading path of the excel file
-    await workbook.xlsx.readFile("C:/Users/avinash.kunapareddy/Downloads/excelDownloadTest.xlsx");
+    await workbook.xlsx.readFile(filepath);
     //accessing first sheet of excel workbook
     const worksheet = workbook.getWorksheet('Sheet1');
+    const output = await readExcel(worksheet, searchText);
+    //getting cell location
+    const cell = worksheet.getCell(output.row, output.col + change.colChange);
+    //replacing required value in cell
+    cell.value = replaceText;
+    //writing replaced cell value to the excel file
+    await workbook.xlsx.writeFile(filepath);
+}
+
+//calling excelTest function
+async function readExcel(worksheet, searchText) {
+    //creating a object with row and column number with default values
+    let output = { row: -1, col: -1 };
     //accessing row by row using rownumber by eachRow function
     worksheet.eachRow((row, rowNumber) => {
         //accessing cell by cell on iterating through column number using eachCell function
         row.eachCell((cell, colNumber) => {
             //comparing cell value with required value 'Apple'
-            if (cell.value === 'Banana') {
+            if (cell.value === searchText) {
                 //storing rowNumber to row and colNumber to col in output object
                 output.row = rowNumber;
                 output.col = colNumber;
@@ -25,13 +37,7 @@ async function excelTest() {
         })
 
     })
-    //getting cell location
-    const cell = worksheet.getCell(output.row, output.col);
-    //replacing required value in cell
-    cell.value = 'Republic';
-    //writing replaced cell value to the excel file
-    await workbook.xlsx.writeFile("C:/Users/avinash.kunapareddy/Downloads/excelDownloadTest.xlsx");
+    return output;
 }
 
-//calling excelTest function
-excelTest();
+writeExcel("Mango", 350, { rowChange: 0, colChange: 2 }, "C:/Users/avinash.kunapareddy/Downloads/excelDownloadTest.xlsx");
